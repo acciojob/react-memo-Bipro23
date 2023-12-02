e722916d7e461221b314ae229c38741e575e6fc4
Bipro23 <88981554+Bipro23@users.noreply.github.com>
2023-12-02 07:27:01 +0000

    submit

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,43 @@
-import React from "react";
-import ToDo from "./Todo";
-import Counter from "./Counter";
-import Memo from "./Memo";
+import React, { useState } from 'react'
 
-const App = () =>{
-    return(
-       <div id="main">
-           <ToDo/>
-           <Counter/>
-           <Memo/>
+const App = () => {
+    const[todo,setTodo] = useState('');
+    const [cnt,setCnt] = useState(0);
+    const[memodata,setMemodata]= useState([]);
+    function handleClick(){
+        var data = document.getElementById('memo-key');
+        if(data.value.length>5){
+            var arr= memodata;
+            arr.push(data.value)
+            setMemodata( arr)
+        }
+        console.log(memodata)
+    }
+   return (
+    <div id='main'>
+        <h1>React.useMemo</h1>
+        <div className='todo'>
+         <p>{todo}</p>
+        <button id='add-todo-btn' onClick={()=>setTodo('New Todo')}>Add Todo</button>
+        </div>
+        <hr></hr>
+        <div className='countnum'>
+         <label>`count:${cnt}`</label>
+         <button id='incr-cnt' onClick={()=> setCnt(cnt+1)}>+</button>
+         <h2>Expensive Calculation</h2>
+         <p>1000000000</p>
+        </div>
+        <hr></hr>
+        <hr></hr>
+       <div className='memo'>
+       <input id='memo-key'/>
+       <button id='skill-input' onClick={handleClick}> Add Skill</button>
+     
+        
+      
        </div>
-    )
+    </div>
+  )
 }
 
-export default App;
+export default App
